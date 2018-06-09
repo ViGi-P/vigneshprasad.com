8753fd730fd4dc888d35e445a1b5523862bc7574
Vigi-P <vigneshdmanfb@gmail.com>
2018-06-09 18:42:02 +0530

    Add spinner

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Spinner = () => <div className='spinner-container'>
+  <div className='spinner'>
+  	<svg viewBox='0 0 66 66' xmlns='http://www.w3.org/2000/svg'>
+  		<circle className='length' fill='none' strokeWidth='8' strokeLinecap='round' cx='33' cy='33' r='28'></circle>
+  	</svg>
+  	<svg viewBox='0 0 66 66' xmlns='http://www.w3.org/2000/svg'>
+  		<circle fill='none' strokeWidth='8' strokeLinecap='round' cx='33' cy='33' r='28'></circle>
+  	</svg>
+  	<svg viewBox='0 0 66 66' xmlns='http://www.w3.org/2000/svg'>
+  		<circle fill='none' strokeWidth='8' strokeLinecap='round' cx='33' cy='33' r='28'></circle>
+  	</svg>
+  	<svg viewBox='0 0 66 66' xmlns='http://www.w3.org/2000/svg'>
+  		<circle fill='none' strokeWidth='8' strokeLinecap='round' cx='33' cy='33' r='28'></circle>
+  	</svg>
+  </div>
+</div>
